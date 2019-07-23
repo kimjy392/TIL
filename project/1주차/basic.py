9879c286210ab802d351a820c6cb286cb0b7cc70
kimjunyoung <kimjy392@gmail.com>
2019-07-23 17:46:39 +0900

    모듈 및 에러

--- project/1주차/basic.py
+++
@@ -1,9 +0,0 @@
-import requests
-key = '2961dcb2e4336ffa43975ece6fdaec32'
-targetDt = '20190713' # yyyymmdd
-api_url = f'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key={key}&targetDt={targetDt}'
-print(api_url)
-response = requests.get(api_url).json()
-print(response)
-
-
