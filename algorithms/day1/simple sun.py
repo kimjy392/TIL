d8f9c7fef7b053681204301c813f258447e6840d
kimjunyoung <kimjy392@gmail.com>
2019-08-05 09:10:21 +0900

    190805 | 저라

--- algorithms/day1/simple sun.py
+++ algorithms/day1/simple sun.py
@@ -1,11 +1,11 @@
-# 중복순열
-arr = "ABC"
-n = len(arr)
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            print(arr[i], arr[j], arr[k])
-
+# # 중복순열
+# arr = "ABC"
+# n = len(arr)
+# for i in range(n):
+#     for j in range(n):
+#         for k in range(n):
+#             print(arr[i], arr[j], arr[k])
+arr = 'ABC'
 # 중복 x
 n = len(arr)
 for i in range(n):
